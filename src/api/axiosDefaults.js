041a2a9a8a2123d00634051cbf5c77519c55ddfa
DanTaylor28 <dan.taylor1493@gmail.com>
2023-03-10 08:37:15 +0000

    Update api url

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://social-media-drf-api.herokuapp.com/";
+axios.defaults.baseURL = "https://drf-api-copy.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
